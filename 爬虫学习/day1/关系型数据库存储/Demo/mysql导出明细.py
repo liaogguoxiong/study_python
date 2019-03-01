ccd4b8037021a20cc0951c4b47768f02d303ca24
liaogguoxiong <47232133+liaogguoxiong@users.noreply.github.com>
2019-03-01 15:21:42 +0800

    再次优化
    优化导出文件的位置

--- 爬虫学习/day1/关系型数据库存储/Demo/mysql导出明细.py
+++ 爬虫学习/day1/关系型数据库存储/Demo/mysql导出明细.py
@@ -42,9 +42,12 @@ def output_data(ip,db_name,port,shuihao,company_name):
         FROM invoice_info A,invoice_info_mx B 
         WHERE A.serial_num=B.serial_num AND nsrsbh='{shuihao}' AND kprq BETWEEN '2019-02-01 00:00:00'AND'2019-02-28 23:59:59' ORDER BY kprq
         '''.format(shuihao=shuihao[name])
+        if os.path.exists('C:/Users/xiao/Desktop/数据导出/{company_name}/开票明细/{filename}.csv'.format(company_name=company_name,filename=name)):
+            print('%s已经存在'%name)
+            continue
         #print(sql)
-        os.mkdir('C:/Users/xiao/Desktop/数据导出/{company_name}/{dicname}'.format(company_name=company_name,dicname=name))
-        f = open('C:/Users/xiao/Desktop/数据导出/{company_name}/{dicname}/{filename}.csv'.format(company_name=company_name,dicname=name,filename=name), 'a', newline='', encoding='utf-8')
+        #os.mkdir('C:/Users/xiao/Desktop/数据导出/{company_name}/开票明细'.format(company_name=company_name))
+        f = open('C:/Users/xiao/Desktop/数据导出/{company_name}/开票明细/{filename}.csv'.format(company_name=company_name,filename=name), 'a', newline='', encoding='utf-8')
         writer = csv.writer(f)
         writer.writerow(
             ['开票日期', '分机号', '发票代码', '发票号码', '开票类型', '原发票代码', '原发票号码', '发票行性质', '项目名称', '规格类型', '单位', '项目数量', '项目单价', '项目金额',
@@ -94,6 +97,81 @@ def main():
         '东莞市黄江东盛加油站有限公司': '914419000506533418'
     }
     output_data('192.168.20.54','dzfp_zzs_kpfw_arm',3306,guande,'冠德')
+
+
+
+    '''
+    ip:192.168.20.54 端口为3306 数据库:dzfp_zzs_kpfw_arm_sign
+
+    '''
+    guande1={
+                '济南鉴德石油化工有限公司':'91370103787448537Q',
+                '山东中惠泽石油有限公司':'91370103575478769F'
+    }
+    output_data('192.168.20.54','dzfp_zzs_kpfw_arm_sign',3306,guande1,'冠德')
+
+
+
+    '''
+        ip:192.168.20.54 端口为3306 数据库:dzfp_zzs_kpfw_arm_sign
+
+    '''
+    jieshun={
+                '厦门市湖里华龙实业公司':'91350206155227568L',
+                '厦门市阳光家园物业服务有限公司海沧分公司':'9135020030325215XA',
+                '广州鼎通晟物业管理有限公司':'914401135721807990',
+                '广州富利物业管理有限公司':'91440106683266317Q',
+                '深圳音乐厅运营管理有限责任公司深圳音乐厅停车场':'91440300685354783N',
+                '广州科建投资管理有限公司':'914401160784081960',
+                '西安市享泰物业管理有限公司':'91610131668680296Q',
+                '安徽八里河旅游开发有限公司':'91341226746788738Y',
+                '深圳市新润园物业发展有限公司理想时代大厦停车场':'91440300MA5DA8U052'
+    }
+    output_data('192.168.20.54','dzfp_zzs_kpfw_arm_sign',3306,jieshun,'捷顺')
+
+
+    # mysqlIP：192.168.20.54/端口3307/Schema：dzfp_zzs_kpfw_arm_drzb
+    jieshun1={
+                '成都新谷投资集团有限公司':'915101007280880172',
+                '成都百扬嘉欣物业服务有限公司':'91510100556400560K'
+    }
+    output_data('192.168.20.54', 'dzfp_zzs_kpfw_arm_drzb', 3307, jieshun1, '捷顺')
+
+
+
+    #   mysqlIP：192.168.20.181/端口3306/Schema：dzfp_zzs_kpfw_arm
+    jieshun2={
+            '深圳市福田农产品批发市场有限公司停车场':'9144030097935050XF'
+    }
+    output_data('192.168.20.181', 'dzfp_zzs_kpfw_arm', 3306, jieshun2, '捷顺')
+
+
+    #   mysqlIP：192.168.20.54/端口3306/Schema：dzfp_zzs_kpfw_arm
+    jieshun3={
+        '陕西鑫华泰物业管理有限公司':'91610104MA6U2GBA1U',
+        '深圳市华侨城物业服务有限公司武汉分公司':'91420100578252853T',
+        '深圳市顺易通信息科技有限公司':'91440300664162117K'
+    }
+    output_data('192.168.20.54', 'dzfp_zzs_kpfw_arm', 3306, jieshun3, '捷顺')
+
+
+    #   mysqlIP：192.168.20.40/端口3306/Schema：dzfp_zzs_kpfw_arm
+    jieshun4={
+        '深圳市信利康物业管理有限公司':'91440300MA5DG1PJ2G'
+    }
+    output_data('192.168.20.40', 'dzfp_zzs_kpfw_arm', 3306, jieshun4, '捷顺')
+
+
+    # mysqlIP：192.168.20.54/端口3306/Schema：dzfp_zzs_kpfw_arm
+    chuoqi={'绰琪服装（深圳）有限公司':'914403007966376128'}
+    output_data('192.168.20.54','dzfp_zzs_kpfw_arm',3306,chuoqi,'绰琪')
+
+    # mysqlIP：192.168.20.40/端口3306/Schema：dzfp_zzs_kpfw_arm
+    huaqiaocheng={
+                '深圳市华侨城物业服务有限公司':'914403001924025138'
+    }
+    output_data('192.168.20.40','dzfp_zzs_kpfw_arm',3306,huaqiaocheng,'华侨城')
+
 
 if __name__ == '__main__':
     main()
